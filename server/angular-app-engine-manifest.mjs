1dc5a8f032e578161cc1d3ddc5bd92cef524d195
misro-888 <giorgimisro@gmail.com>
2026-05-04 20:43:23 +0400

    Auto-generated commit

---
+++ server/angular-app-engine-manifest.mjs
@@ -0,0 +1,11 @@
+
+export default {
+  basePath: '/angular-ecommerce-final',
+  allowedHosts: [],
+  supportedLocales: {
+  "en-US": ""
+},
+  entryPoints: {
+    '': () => import('./main.server.mjs')
+  },
+};
---
+++ server/assets-chunks/index_csr_html.mjs
@@ -0,0 +1,14 @@
+export default `<!doctype html>
+<html lang="en" data-beasties-container="">
+<head>
+  <meta charset="utf-8">
+  <title>AngularEcommerce</title>
+  <base href="/angular-ecommerce-final/">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/x-icon" href="favicon.ico">
+<style>*{box-sizing:border-box}body{margin:0;font-family:Arial,sans-serif;background:#f3f4f6;color:#111827}</style><link rel="stylesheet" href="styles-EFRJXCO4.css" media="print" onload="this.media='all'"><noscript><link rel="stylesheet" href="styles-EFRJXCO4.css"></noscript></head>
+<body ngcm="">
+  <app-root></app-root>
+<script src="main-OPHTEZGL.js" type="module"></script></body>
+</html>
+`;
